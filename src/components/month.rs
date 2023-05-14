c2d3a45ae38c94bcb092cb9277d962dca8f98a74
Samantha <sam.t.nguyenn@gmail.com>
2023-05-14 16:02:57 -0500

    tests: add unit tests for month format

--- src/components/month.rs
+++ src/components/month.rs
@@ -25,6 +25,10 @@ pub fn parse_month_component(s: &str, position: &mut usize) -> Option<(u32, u8)>
 	}
 
 	let parsed_month = collect_ascii_digits(s, position);
+	if parsed_month.len() != 2 {
+		return None;
+	}
+
 	let month = parsed_month.parse::<u8>().ok()?;
 	if !is_valid_month(&month) {
 		return None;
@@ -35,7 +39,25 @@ pub fn parse_month_component(s: &str, position: &mut usize) -> Option<(u32, u8)>
 
 #[cfg(test)]
 mod tests {
-	use super::parse_month_component;
+	use super::{parse_month_component, parse_month_string};
+
+	#[test]
+	fn test_parse_month_string() {
+		let parsed = parse_month_string("2004-12");
+		assert_eq!(parsed, Some((2004, 12)));
+	}
+
+	#[test]
+	fn test_parse_month_string_fails_invalid_month() {
+		let parsed = parse_month_string("2004-2a");
+		assert_eq!(parsed, None);
+	}
+
+	#[test]
+	fn test_parse_month_string_fails() {
+		let parsed = parse_month_string("2004-13");
+		assert_eq!(parsed, None);
+	}
 
 	#[test]
 	fn test_parse_month_component() {
@@ -43,5 +65,45 @@ mod tests {
 		let parsed = parse_month_component("2004-12", &mut position);
 
 		assert_eq!(parsed, Some((2004, 12)));
+	}
+
+	#[test]
+	fn test_parse_month_component_fails_year_lt_4_digits() {
+		let mut position = 0usize;
+		let parsed = parse_month_component("200-12", &mut position);
+
+		assert_eq!(parsed, None);
+	}
+
+	#[test]
+	fn test_parse_month_component_fails_invalid_month_lower_bound() {
+		let mut position = 0usize;
+		let parsed = parse_month_component("2004-0", &mut position);
+
+		assert_eq!(parsed, None);
+	}
+
+	#[test]
+	fn test_parse_month_component_fails_invalid_month_upper_bound() {
+		let mut position = 0usize;
+		let parsed = parse_month_component("2004-13", &mut position);
+
+		assert_eq!(parsed, None);
+	}
+
+	#[test]
+	fn test_parse_month_component_fails_invalid_month_syntax() {
+		let mut position = 0usize;
+		let parsed = parse_month_component("2004-1a", &mut position);
+
+		assert_eq!(parsed, None);
+	}
+
+	#[test]
+	fn test_parse_month_component_fails_invalid_separator() {
+		let mut position = 0usize;
+		let parsed = parse_month_component("2004/12", &mut position);
+
+		assert_eq!(parsed, None);
 	}
 }
