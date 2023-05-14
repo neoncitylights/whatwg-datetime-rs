b07d622f6a134bb8741cb6ca68a4bce71c00962d
Samantha <sam.t.nguyenn@gmail.com>
2023-05-14 14:26:06 -0500

    chore: split up date component in own file

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,5 +1,7 @@
+mod date;
 mod month;
 mod yearless_date;
 
+pub use self::date::*;
 pub use self::month::*;
 pub use self::yearless_date::*;
--- src/lib.rs
+++ src/lib.rs
@@ -60,40 +60,6 @@ pub(crate) enum DurationUnit {
 	Second,
 }
 
-pub fn parse_date_string(s: &str) -> Option<(u32, u8, u8)> {
-	let mut position = 0usize;
-	let parsed = parse_date_component(s, &mut position)?;
-	if position < s.len() {
-		return None;
-	}
-
-	Some(parsed)
-}
-
-pub fn parse_date_component(s: &str, position: &mut usize) -> Option<(u32, u8, u8)> {
-	let (year, month) = parse_month_component(s, position)?;
-
-	if *position > s.len() || s.chars().nth(*position) != Some(TOKEN_DATETIME_SEPARATOR) {
-		return None;
-	} else {
-		*position += 1;
-	}
-
-	let parsed_day = collect_ascii_digits(s, position);
-	if parsed_day.len() != 2 {
-		return None;
-	}
-
-	let max_day = max_days_in_month_year(month, year)?;
-	let day = parsed_day.parse::<u8>().ok()?;
-
-	if !(1..=max_day).contains(&day) {
-		return None;
-	}
-
-	Some((year, month, day))
-}
-
 pub fn parse_time_component(s: &str, position: &mut usize) -> Option<NaiveTime> {
 	let parsed_hour = collect_ascii_digits(s, position);
 	if parsed_hour.len() != 2 {
@@ -484,21 +450,12 @@ mod tests {
 		NaiveDate,
 		NaiveDateTime,
 		NaiveTime,
-		parse_date_component,
 		parse_local_datetime,
 		parse_time_component,
 		parse_timezone_offset_component,
 		TimeZoneOffset,
 		YearWeek,
 	};
-
-	#[test]
-	fn test_parse_date_component() {
-		let mut position = 0usize;
-		let parsed = parse_date_component("2004-12-31", &mut position);
-
-		assert_eq!(parsed, Some((2004, 12, 31)));
-	}
 
 	#[test]
 	fn test_parse_time_component() {
